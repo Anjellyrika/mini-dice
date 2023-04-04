fd5c0fe98e7fae3128446757a5270913ef8bd751
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-04-04 13:57:45 +0800

    feat: add random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use std::io;
+use rand::{Rng, thread_rng};
 
 fn main() {
     println!("Die sizes: [d4] [d6] [d8] [d10] [d12] [d20]");
     println!("Input the die size:");
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    print!("Rolling a d{}", input);
+
+    let die_size: u32 = input.trim().parse().unwrap();
+    println!("Rolling a d{}", die_size);
+
+    let mut rng = thread_rng();
+    let result: u32 = rng.gen_range(1..=die_size);
+    println!("You got a {}", result);
 }
+
