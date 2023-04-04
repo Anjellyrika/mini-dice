6c5a7e65fb4f7f311d7302a9e35f34d255150b63
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-04-04 18:06:47 +0800

    build: add egui dependency

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,5 @@ fn main() {
     let die_size: u32 = input.trim().parse().unwrap();
     println!("Rolling a d{}", die_size);
 
-    let mut rng = thread_rng();
-    let result: u32 = rng.gen_range(1..=die_size);
-
-    if die_size == 20 {
-        match result {
-            1 => println!("You got a Nat 1..."),
-            20  => println!("You got a Nat 20!"),       
-            8 => println!("You got an 8."),
-            11 => println!("You got an 11."),
-            18 => println!("You got an 18."),
-            _ => println!("You got a {}.", result),
-        };
-    }
-
-    else {
-        match result {
-           8 => println!("You got an 8."),
-           11 => println!("You got an 11."),
-           18 => println!("You got an 18."),
-           _ => println!("You got a {}.", result),
-        };
-    }
+    roller(die_size);
 }
-
