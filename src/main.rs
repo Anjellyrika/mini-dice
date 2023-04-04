cfb50f05f96f12393f581f5b8397026b7423b382
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-04-04 14:06:27 +0800

    feat: add custom messages for die roll result

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,25 @@ fn main() {
 
     let mut rng = thread_rng();
     let result: u32 = rng.gen_range(1..=die_size);
-    println!("You got a {}", result);
+
+    if die_size == 20 {
+        match result {
+            1 => println!("You got a Nat 1..."),
+            20  => println!("You got a Nat 20!"),       
+            8 => println!("You got an 8."),
+            11 => println!("You got an 11."),
+            18 => println!("You got an 18."),
+            _ => println!("You got a {}.", result),
+        };
+    }
+
+    else {
+        match result {
+           8 => println!("You got an 8."),
+           11 => println!("You got an 11."),
+           18 => println!("You got an 18."),
+           _ => println!("You got a {}.", result),
+        };
+    }
 }
 
