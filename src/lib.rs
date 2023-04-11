e656f2ba2c42542136bc36f6f4796d54de85c88a
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-04-12 04:14:01 +0800

    refactor: match against tuple for the custom message

--- src/lib.rs
+++ src/lib.rs
@@ -4,27 +4,17 @@ use rand::{thread_rng, Rng};
 pub use state::State;
 
 pub fn message(die_size: u32, die_result: u32) -> String {
-    let msg = match die_result {
-            1 => {
-                if die_size == 20 {
-                    String::from("You got a Nat 1...")
-                } else {
-                    String::from("You got a 1.")
-                }
-            },
-            20 => {
-                if die_size == 20 {
-                    String::from("You got a Nat 20!")
-                } else {
-                    String::from("You got a 20.")
-                }
-            },
-            8 => String::from("You got an 8."),
-            11 => String::from("You got an 11."),
-            18 => String::from("You got an 18."),
-            _ => format!("You got a {}.", die_result),
-        };
-        return msg;
+    match (die_size, die_result) {
+        (20, 1) => String::from("You got a Nat 1..."),
+        (_, 1) => String::from("You got a 1."),
+        (20, 20) => String::from("You got a Nat 20!"),
+        (_, 20) => String::from("You got a 20."),
+        (_, 8) => String::from("You got an 8."),
+        (_, 11) => String::from("You got an 11."),
+        (_, 18) => String::from("You got an 18."),
+        (100, eighties @ 80..=89) => format!("You got an {eighties}"),
+        _ => format!("You got a {die_result}."),
+    }
 }
 
 pub fn roller(die_size: u32) -> u32 {
