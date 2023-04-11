830d36dc3859d909a4fcb282b812bfd9ac8e9fd8
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-04-12 01:32:30 +0800

    perf: cache text output

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ use mini_dice_roller::{State, roller, message};
 struct DiceRoller {
     current_state: State,
     die_size: Option<u32>,
-    die_result: Option<u32>
+    die_result: Option<u32>,
+
+    // Text display
+    rolling_msg: String,
+    result_msg: String
 }
 
 impl eframe::App for DiceRoller {
@@ -23,14 +27,14 @@ impl eframe::App for DiceRoller {
             let d100 = ui.button("D100");
 
             let maybe_reset = if let Some(die_size) = self.die_size {
-                // FIXME: Potential performance bottleneck due to frequent `malloc` + `free`!
-                // => Cache the output in the DiceRoller struct
-                ui.label(format!("Rolling d{die_size}..."));
-                ui.label(message(self.die_size.unwrap_or_default(), self.die_result.unwrap_or_default()));
+                ui.label(&self.rolling_msg);
+                ui.label(&self.result_msg);
                 
                 match self.current_state {
                     State::Selection => {
+                        self.rolling_msg = format!("Rolling d{die_size}...");
                         self.die_result = Some(roller(die_size));
+                        self.result_msg = message(self.die_size.unwrap(), self.die_result.unwrap());
                         self.current_state = State::Result;
                     },
                     State::Result => {
@@ -63,8 +67,7 @@ impl eframe::App for DiceRoller {
             } else if d100.clicked() {
                 self.current_state = State::Selection;
                 Some(100)
-            }
-             else if maybe_reset.as_ref().map(egui::Response::clicked).unwrap_or_default() {
+            } else if maybe_reset.as_ref().map(egui::Response::clicked).unwrap_or_default() {
                 self.current_state = State::Selection;
                 None
             } else {
